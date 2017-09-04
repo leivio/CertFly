e2348aafadf065360184ec4a16744c998a813b19
Leivio Fontenele <leivio@datafocussistemas.com.br>
2017-09-04 10:11:39 -0300

    inclusão dos termos da Licença Pública Geral Menor do GNU 2.1

--- Certfly/Certfly.Assinar.cs
+++ Certfly/Certfly.Assinar.cs
@@ -1,4 +1,37 @@
-﻿using System.Security.Cryptography;
+﻿/********************************************************************************/
+/* Projeto: Componente CertFly                                                  */
+/* Proposta: Lib para assinar documentos XML com A3 e Sha256 para todas as      */
+/* liguagens de programação com suporte a COM                                   */
+/* Endereço - https://github.com/leivio/CertFly                                 */
+
+/* Direitos Autorais Reservados (c) 2017 Leivio Fontenele                       */
+
+/* Colaboradores nesse arquivo:                                                 */
+
+/*  Você pode obter a última versão desse arquivo na pagina do Projeto ACBr     */
+/* Componente localizado em https://github.com/leivio/CertFly                   */
+
+
+/*  Esta biblioteca é software livre; você pode redistribuí-la e/ou modificá-la */
+/* sob os termos da Licença Pública Geral Menor do GNU conforme publicada pela  */
+/* Free Software Foundation; tanto a versão 2.1 da Licença, ou (a seu critério) */
+/* qualquer versão posterior.                                                   */
+
+/*  Esta biblioteca é distribuída na expectativa de que seja útil, porém, SEM   */
+/* NENHUMA GARANTIA; nem mesmo a garantia implícita de COMERCIABILIDADE OU      */
+/* ADEQUAÇÃO A UMA FINALIDADE ESPECÍFICA. Consulte a Licença Pública Geral Menor*/
+/* do GNU para mais detalhes. (Arquivo LICENÇA.TXT ou LICENSE.TXT)              */
+
+/*  Você deve ter recebido uma cópia da Licença Pública Geral Menor do GNU junto*/
+/* com esta biblioteca; se não, escreva para a Free Software Foundation, Inc.,  */
+/* no endereço 59 Temple Street, Suite 330, Boston, MA 02111-1307 USA.          */
+/* Você também pode obter uma copia da licença em:                              */
+/* http://www.opensource.org/licenses/lgpl-license.php                          */
+
+/* Leivio Ramos de Fontenele  -  leivio@yahoo.com.br  -  www.happysale.com.br   */
+/********************************************************************************/
+
+using System.Security.Cryptography;
 using System.Security.Cryptography.Xml;
 using System.Security.Cryptography.X509Certificates;
 using System.Xml;
